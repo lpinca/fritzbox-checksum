b5a1ff1586c2a080848bdce34b30da136bb21a77
Luigi Pinca <luigipinca@gmail.com>
2021-05-06 12:44:41 +0200

    [test] Increase code coverage

--- test/test.js
+++ test/test.js
@@ -68,3 +68,51 @@ test('honors the --print-config argument', function (t) {
 
   source.pipe(subprocess.stdin);
 });
+
+test('honors the --output argument', function (t) {
+  const source = fs.createReadStream(
+    path.join(
+      __dirname,
+      'fixtures',
+      'FRITZ.Box 7490 113.07.12i_01.01.70_0106.export'
+    )
+  );
+
+  const subprocess = fork(
+    path.join(__dirname, '..', 'cli.js'),
+    ['--output', process.platform === 'win32' ? '\\\\.\\nul' : '/dev/null'],
+    {
+      stdio: 'pipe'
+    }
+  );
+
+  subprocess.on('close', function (code) {
+    t.equal(code, 0);
+    t.end();
+  });
+
+  source.pipe(subprocess.stdin);
+});
+
+test('honors the FILE argument', function (t) {
+  const file = path.join(
+    __dirname,
+    'fixtures',
+    'FRITZ.Box 7490 113.07.21i_01.01.70_0111.export'
+  );
+
+  const chunks = [];
+  const subprocess = fork(path.join(__dirname, '..', 'cli.js'), [file], {
+    stdio: 'pipe'
+  });
+
+  subprocess.stdout.on('data', function (chunk) {
+    chunks.push(chunk);
+  });
+
+  subprocess.on('close', function (code) {
+    t.equal(code, 0);
+    t.deepEqual(Buffer.concat(chunks), Buffer.from('448A9496\n'));
+    t.end();
+  });
+});
